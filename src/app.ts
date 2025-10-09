41cdd0cf60719ee461059848c2bbd1b94601aa94
Samit Koyom <samit90daytalk@hotmail.com>
2025-10-10 05:39:30 +0700

    update app.ts file

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,8 @@ app.get('/api/users', (_: Request, res: Response) => {
 app.get('/api/products', (_: Request, res: Response) => {
   const products = [
     { id: 1, name: 'Product A', price: 100 },
-    { id: 2, name: 'Product B', price: 150 }
+    { id: 2, name: 'Product B', price: 150 },
+    { id: 3, name: 'Product C', price: 200 }
   ]
   res.json(products)
 })
