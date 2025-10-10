69f2006af11dbb006b1266a2852641eae3b10e61
Samit Koyom <samit90daytalk@hotmail.com>
2025-10-10 15:42:01 +0700

    Test Jenkins CI/CD

--- src/app.ts
+++ src/app.ts
@@ -40,7 +40,8 @@ app.get('/api/products', (_: Request, res: Response) => {
   const products = [
     { id: 1, name: 'Product A', price: 100 },
     { id: 2, name: 'Product B', price: 150 },
-    { id: 3, name: 'Product C', price: 200 }
+    { id: 3, name: 'Product C', price: 200 },
+    { id: 4, name: 'Product D', price: 250 }
   ]
   res.json(products)
 })
